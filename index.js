fb7e84d0bdfd31a04ca3df0938f03202dbbe72f9
udaysharma1501 <udaysharma1501@gmail.com>
2024-07-26 19:30:24 +0530

    basic setup 3

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(port, () => console.log(`server started on port: ${port}`));
+app.listen(port, () => console.log(`server started on port: ${port}`))
